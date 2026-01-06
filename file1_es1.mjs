7b03706e11101b05897a9b93cb8746eed66a0360
Mahfuj Abdulla <155483252+IamFreak10@users.noreply.github.com>
2026-01-06 08:18:20 +0600

    esm

---
+++ file1_es1.mjs
@@ -0,0 +1,10 @@
+// common js to esm
+
+import {a as a} from './file2_es2.mjs';
+
+import {a as x} from './file3_es3.mjs';
+// const {add} = require('./utils/add');
+// const{subs}=require('./utils/subs');
+// const{add}=require('./utils/index');
+
+console.log(a,x);
