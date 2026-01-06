8dd9f23325e3208ec627f8c95585d39f6402f02e
Mahfuj Abdulla <155483252+IamFreak10@users.noreply.github.com>
2026-01-06 07:42:23 +0600

    First Commit

---
+++ file1.js
@@ -0,0 +1,8 @@
+const {a}=require('./file2');
+const {a:x}=require('./file3');
+// const {add} = require('./utils/add');
+// const{subs}=require('./utils/subs');
+const{add}=require('./utils/index');
+
+console.log(add(a,x));
+// console.log(module);
